7814254c050681a1aa50092bdcc8ba22a6bf61b6
anzohra@outlook.com <anzohra@outlook.com>
2018-04-05 02:59:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 function theBeatlesPlay(musicians, instruments) {
-  function
+  const arr = []
+
+  for (let i = 0, l = musicians.length; i < l; i++) {
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+
+  return arr
 }
