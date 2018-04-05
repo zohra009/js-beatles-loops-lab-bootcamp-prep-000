d24648b6a5f72e404b66ca8441f6d42d8478c35b
anzohra@outlook.com <anzohra@outlook.com>
2018-04-05 03:00:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,25 @@ function theBeatlesPlay(musicians, instruments) {
 
   return arr
 }
+function johnLennonFacts(facts) {
+  const shoutedFacts = []
+
+  let i = 0
+  while (i < facts.length) {
+    shoutedFacts.push(`${facts[i]}!!!`)
+    i++
+  }
+
+  return shoutedFacts
+}
+
+function iLoveTheBeatles(n) {
+  const arr = []
+
+  do {
+    arr.push('I love the Beatles!')
+    n++
+  } while (n < 15)
+
+  return arr
+}
